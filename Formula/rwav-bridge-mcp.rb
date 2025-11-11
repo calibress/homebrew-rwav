237b952ca8aa9072dce9da32f227e4e8473ddd77
Craig Prescott <craig@calibresoftwaresystems.co.uk>
2025-11-11 22:22:14 +0000

    rwav-bridge-mcp 0.2.11 (#19)

--- Formula/rwav-bridge-mcp.rb
+++ Formula/rwav-bridge-mcp.rb
@@ -1,8 +1,8 @@
 class RwavBridgeMcp < Formula
   desc "RWAV Bridge MCP server"
   homepage "https://github.com/calibress/rwav-mcp-bridge"
-  url "https://github.com/calibress/rwav-mcp-bridge/releases/download/v0.2.10/rwav-bridge-mcp-v0.2.10.tar.gz"
-  sha256 "0b6c4fdafa785a4c17dde0cf5b75a5d919268c43aa232286896ead6124099d21"
+  url "https://github.com/calibress/rwav-mcp-bridge/releases/download/v0.2.11/rwav-bridge-mcp-v0.2.11.tar.gz"
+  sha256 "706f396ab8e52700c736b3e1b80ec53606bb53846487a2b6429220f2dd1c65e5"
   license :cannot_represent
 
   depends_on "node"
@@ -22,6 +22,6 @@ class RwavBridgeMcp < Formula
 
   test do
     output = shell_output("#{bin}/rwav-bridge-mcp --version")
-    assert_match "0.2.10", output
+    assert_match "0.2.11", output
   end
 end
